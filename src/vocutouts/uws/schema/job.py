4460ef0ff6e7831a7f28c8bfa055b0b736d8fbe4
Russ Allbery <rra@lsst.org>
2023-01-30 14:15:37 -0800

    Fix type annotations for new SQLAlchemy

--- src/vocutouts/uws/schema/job.py
+++ src/vocutouts/uws/schema/job.py
@@ -11,7 +11,7 @@ from __future__ import annotations
 from datetime import datetime
 
 from sqlalchemy import Column, DateTime, Enum, Index, Integer, String, Text
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped, relationship
 
 from ..models import ErrorCode, ErrorType, ExecutionPhase
 from .base import Base
@@ -40,10 +40,10 @@ class Job(Base):
     error_message: str | None = Column(Text)
     error_detail: str | None = Column(Text)
 
-    parameters: list[JobParameter] = relationship(
+    parameters: Mapped[list[JobParameter]] = relationship(
         "JobParameter", cascade="delete", lazy="selectin", uselist=True
     )
-    results: list[JobResult] = relationship(
+    results: Mapped[list[JobResult]] = relationship(
         "JobResult", cascade="delete", lazy="selectin", uselist=True
     )
 
