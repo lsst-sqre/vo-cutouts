9795d9b881277be1e2ef4f6c10f54d34c1faa3da
Russ Allbery <rra@lsst.org>
2024-08-02 12:11:39 -0700

    Regenerate dependencies with released Safir
    Safir 6.2.0 has been released with UWS library support. Update and
    regenerate dependencies accordingly.

--- scripts/install-worker.sh
+++ scripts/install-worker.sh
@@ -33,5 +33,5 @@ scons install declare -t current
 
 # Install Python dependencies and the vo-cutouts code.
 cd "$1"
-pip install --no-cache-dir google-cloud-storage httpx 'safir-arq @ git+https://github.com/lsst-sqre/safir@main#subdirectory=safir-arq'
+pip install --no-cache-dir google-cloud-storage httpx safir-arq structlog
 pip install --no-cache-dir --no-deps .
